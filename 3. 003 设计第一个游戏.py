163cb7057311d134265ef28640c814e22bade578
Xin1991 <32119591+Xin1991@users.noreply.github.com>
2017-10-23 08:16:42 +0800

    Add files via upload

---
+++ 3. 003 设计第一个游戏.py
@@ -0,0 +1,9 @@
+print('----i love xinxin----')
+temp =input('你猜猜我心里现在是那个数字:')
+guess = int(temp)
+if guess == 8:
+    print("卧槽，你是我的优乐美吗？！")
+    print("哼，猜中了也没有奖励！")
+else:
+    print ("哼，我这么爱你你都猜不中！离婚离婚！")
+print("游戏结束，不玩了！")
